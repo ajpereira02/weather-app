f40e2d0b95fb4759c626acf7896848ecf4841a3c
Ashley Pereira <ashleypereira70@gmail.com>
2019-03-22 15:24:31 +0000

    created 3 components, added the API key to render

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+
+
+const API_Key = "e030cfa869de1bc91eac73cd34a4a510";
 
 class App extends React.Component {
+    getWeather = async () => {
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_Key}
+        &units=metric`);
+        const data = await api_call.json();
+        console.log(data);
+    }
     render() {
         return (
-            <div>Hello!</div>
+            <div>
+                <Titles />
+                <Form />
+                <Weather />
+            </div>
         );
     }
 }
 
+export default App;

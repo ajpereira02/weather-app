9ddf84f8d161e966b7dd4a3b90fc36e0d3ce25de
Ashley Pereira <ashleypereira70@gmail.com>
2019-03-25 08:54:59 +0000

    Styling added to the keys and values of the result

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,11 +2,31 @@ import React from "react";
 
 const Weather = props => (
     <div className="wether__info">
-        { props.city && props.country && <p>Location: { props.city }, { props.country }</p> }
-        { props.temperature && <p>Temperature: { props.temprature }</p> }
-        { props.humidity && <p>Humidity: { props.humidity }</p> }
-        { props.description && <p>Conditions: { props.description }</p> }
-        { props.error && <p>{ props.error }</p> }
+        { 
+            props.city && props.country && <p className="weather__key">Location: 
+            <span className="weather__value"> { props.city }, { props.country }</span>
+        </p> 
+        }
+        { 
+            props.temperature && <p className="weather__key">Temperature: 
+            <span className="weather__value"> { props.temprature }</span>
+        </p> 
+        }
+        { 
+            props.humidity && <p className="weather__key">Humidity: 
+            <span className="weather__value"> { props.humidity }</span>
+        </p> 
+        }
+        { 
+            props.description && <p className="weather__key">Conditions: 
+            <span className="weather__value"> { props.description }</span>
+        </p> 
+        }
+        { 
+            props.error && <p className="weather__error">
+            <span>{ props.error }</span>
+        </p> 
+        }
     </div>
 );
 
